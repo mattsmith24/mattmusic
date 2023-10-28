8a43400945ff098d7dd7464e362bed8a20e85e5c
Matthew Smith <yo.checkit@gmail.com>
2023-10-28 21:05:13 +1000

    Add 'dc' support to Mix

--- src/mix.rs
+++ src/mix.rs
@@ -3,6 +3,7 @@ pub mod mix {
     use crate::read_song::read_song::SongReader;
     use crate::traits::traits::{SoundSource, DynSoundSource, SoundData};
 
+    use crate::dc::dc::DC;
     use crate::sequence::sequence::Sequence;
 
     #[derive(Clone)]
@@ -41,8 +42,30 @@ pub mod mix {
 
         fn from_yaml(params: &Vec::<String>, reader: &mut SongReader) -> DynSoundSource {
             let mut mix = Mix::new();
+            let mut max_duration = 0.0;
             for param in params {
-                mix.add(reader.get_sound(param));
+                println!("Mix::from_yaml(param: {})", param);
+                let parts: Vec<_> = param.split(" ").collect();
+                // If the first token is 'dc' then we expect the following to be the value and duration
+                // Otherwise we expect to see a dc offset and a source name
+                if parts[0] == "dc" {
+                    let val = parts[1].parse::<f32>().unwrap();
+                    let duration: f32;
+                    // If the dc component duration token is "max" then we use the
+                    // running maximum duration of any previous sources.
+                    if parts[2] == "max" {
+                        duration = max_duration;
+                        println!("max: duration = {}", duration);
+                    } else {
+                        duration = parts[2].parse::<f32>().unwrap() * reader.sample_rate as f32;
+                    }
+                    let source = Box::new(DC::new(val, duration.round() as i32));
+                    mix.add(source);
+                } else {
+                    let source = reader.get_sound(param);
+                    max_duration = max_duration.max(source.duration() as f32);
+                    mix.add(source);
+                }
             }
             Box::new(mix)
         }
